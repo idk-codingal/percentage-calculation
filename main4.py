2bc99aeef4eff036bfda931360f7aa31305e765a
idk-codingal <mokshnagda2020@gmail.com>
2024-12-11 17:32:49 -0800

    first commit

---
+++ main4.py
@@ -0,0 +1,10 @@
+print("Enter marks obtained in four subjects")
+Math = int(input("Math :"))
+Social_Studies = int(input("Social_Studies :"))
+English = int(input("English :"))
+Science = int(input("Science :"))
+sum = Math+Social_Studies+English+Science
+print("Total of your subjects is :")
+perc = (sum/400)*100
+print("percentage mark")
+print("perc")
